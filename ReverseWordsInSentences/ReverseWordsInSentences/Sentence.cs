58f6b97223756349fcbd0b190a6847b69d1c354d
Roberto Carrillo <email@robertocarrillo.com>
2015-05-11 00:59:17 -0500

    Initial version of algorithm
    This is the naive implementation of how to reverse words in a string
    using the same amount of memory (not copying data)

---
+++ ReverseWordsInSentences/ReverseWordsInSentences/Sentence.cs
@@ -0,0 +1,68 @@
+﻿namespace ReverseWordsInSentences
+{
+    internal class Sentence
+    {
+        private readonly char[] _input;
+
+        public Sentence(char[] input)
+        {
+            _input = input;
+        }
+
+        public void ReverseWords()
+        {
+            // 1. Reverse all characters
+            ReversePhrase(0, _input.Length);
+
+            // 2. Reverse word by word
+            int start = 0;
+
+            for (int i = 0; i < _input.Length; i++)
+            {
+                if (_input[i] == ' ')
+                {
+                    ReverseWord(start, i - 1);
+                    start = i + 1;
+                }
+
+                if (_input.Length == i + 1)
+                {
+                    ReverseWord(start, i);
+                }
+            }
+        }
+
+        private void ReversePhrase(int start, int end)
+        {
+            int counter = end - 1;
+            for (int i = start; i < end/2; i++)
+            {
+                Swap(i, counter);
+                counter--;
+            }
+        }
+
+        private void ReverseWord(int start, int end)
+        {
+            int counter = end + 1;
+            for (int i = start; i <= end; i++)
+            {
+                Swap(i, counter - 1);
+                counter--;
+
+                if (counter - 1 == i)
+                    break;
+
+                if (i == counter)
+                    break;
+            }
+        }
+
+        private void Swap(int positionA, int positionB)
+        {
+            char temp = _input[positionA];
+            _input[positionA] = _input[positionB];
+            _input[positionB] = temp;
+        }
+    }
+}
---
+++ ReverseWordsInSentences/ReverseWordsInSentences/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ReverseWordsInSentences
+{
+    [TestFixture]
+    class Tests
+    {
+        [TestCase("", "")]
+        [TestCase("Hello World Roberto", "Roberto World Hello")]
+        [TestCase("abc abc", "abc abc")]
+        [TestCase("foo bar", "bar foo")]
+        [TestCase("foos bars", "bars foos")]
+        [TestCase("ab", "ab")]
+        public void Should_reverse_words(string original, string expected)
+        {
+            // Arrange
+            char[] array = original.ToArray();
+
+            // Act
+            var sentence = new Sentence(array);
+            sentence.ReverseWords();
+
+            // Assert
+            string actual = new string(array);
+            Assert.That(array, Is.EqualTo(expected),
+                string.Format("Expected: '{0}'\n  Actual:   '{1}'\n             123456789012345678901234567890\n             0        1         2", expected, actual));
+        }
+    }
+}
